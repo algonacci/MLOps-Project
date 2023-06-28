3c145f0a9103406bc745c6f42e53e7b77b1e88af
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-06-28 22:26:31 +0700

    updates

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import pickle
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TextInput(BaseModel):
+    text: str
+
+model = pickle.load(open('spam.pkl', 'rb'))
+cv = pickle.load(open('vectorizer.pkl', 'rb'))
 
 @app.get("/")
 def index():
@@ -10,6 +18,16 @@ def index():
             "message": "Success fetching the API"
         }
     }
+
+@app.post("/predict")
+def predict_spam(text_input: TextInput):
+    data = [text_input.text]
+    vec = cv.transform(data).toarray()
+    result = model.predict(vec)
+    if result[0] == 0:
+        return {"prediction": "This is Not A Spam Email"}
+    else:
+        return {"prediction": "This is A Spam Email"}
 
 if __name__ == "__main__":
     import uvicorn
